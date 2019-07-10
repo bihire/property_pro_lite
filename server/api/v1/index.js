ff9a98427c7e81386002b237c0ed4818f54765af
bihire <muhireboris@yahoo.fr>
2019-07-10 13:28:56 +0200

    feat(server): add a flag on a property - any user can flag a proprety [Finishes #167194791]

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,9 +1,9 @@
 const authRoutes = require('./routes/auth');
 const propertyRoutes = require('./routes/property');
-// const flagRoutes = require('./routes/flag');
+const flagRoutes = require('./routes/flag');
 
 module.exports = (app) => {
   app.use('/api/v1', authRoutes);
   app.use('/api/v1', propertyRoutes);
-  // app.use('/api/v1', flagRoutes);
+  app.use('/api/v1', flagRoutes);
 };
