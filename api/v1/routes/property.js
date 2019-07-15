2259120c7d2d50d7b03e4282ca5095e355642743
boris bihire <37307172+bihire@users.noreply.github.com>
2019-07-15 12:29:52 +0200

    show the error to LFA
    - comment where the error is

--- api/v1/routes/property.js
+++ api/v1/routes/property.js
@@ -6,12 +6,12 @@ const PropertyController = require('../controllers/propertyController');
   router.post('/property', jwt, PropertyController.add);
   router.patch('/property/:property_id', jwt, PropertyController.update);
   router.patch('/property/:property_id/sold', jwt, PropertyController.update);
-  router.get('/property/:property_id', PropertyController.get);
+  router.get('/property/:property_id', PropertyController.get);  // Goes here
   router.get('/property/user/:owner', PropertyController.get_by_user);
   router.get('/property/allproperties/g', PropertyController.get_self);
 
   router.delete('/property/:property_id', jwt, PropertyController.delete);
   
-  router.get('/properti/bro', PropertyController.getAllProperties);
+  router.get('/properti/bro', PropertyController.getAllProperties);   // Here when i change the path to '/property/something' it goes to router.get('/property/:property_id', PropertyController.get) callbacks
 
   module.exports = router;
